3cd31d737eee5e270d9700e876fa2a65b9f3c6c9
Alvaro Javier Perez Salcedo <japersa92@gmail.com>
2020-07-01 17:11:57 -0500

    Weather api documentation

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -5,12 +5,30 @@ use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    
+     /**
+     * @OA\Get(
+     *      path="/api/search/city/{city}",
+     *      description="Return list of cities by search criteria",
+     *      @OA\Parameter(
+     *          name="city",
+     *          description="city name",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="List of cities",
+     *       ),
+     * )
+     */
     public function showCity($city)
     {
         $client = new \GuzzleHttp\Client();
-        $uri = "https://api.mapbox.com/geocoding/v5/mapbox.places";
-        $access_token = "pk.eyJ1IjoiamFwZXJzYTkyIiwiYSI6ImNrYnlhMzk1NjB4bjgzMHAzbXc3YWoxbzIifQ.J4iCR7WcneWQne0upHC2Ew";
+        $uri = env('API_MAPBOX');
+        $access_token = env('ACCESS_TOKEN_MAPBOX');
         $response = $client->request('GET', "{$uri}/{$city}.json?access_token={$access_token}");
 
         if ($response->getBody()) {
@@ -19,10 +37,39 @@ class ApiController extends Controller
         } 
     }
     
+        /**
+     * @OA\Get(
+     *      path="/api/weather/{lat}/{long}",
+     *      description="Returns information weather of a city by latitude and longitude",
+     *      @OA\Parameter(
+     *          name="lat",
+     *          description="latitud",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),*     
+     *      @OA\Parameter(
+     *          name="long",
+     *          description="longitude",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Return weather information",
+     *       ),
+     * )
+     */
     public function showWeather($lat, $long)
     {
         $client = new \GuzzleHttp\Client();
-        $response = $client->request('GET', "https://api.darksky.net/forecast/88030114c5e47763a011a75e7a10c633/{$lat},{$long}");
+        $uri = env('API_DARKSKY');
+        $response = $client->request('GET', "{$uri}/{$lat},{$long}");
 
         if ($response->getBody()) {
             $body = json_decode($response->getBody());
